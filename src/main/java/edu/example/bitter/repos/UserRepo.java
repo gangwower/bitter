d06f5926eae20d941bf63ebfe2d1043c8c95dee7
gangwower <gangwower@gmail.com>
2020-06-11 06:17:32 +0700

    Added login and registration

---
+++ src/main/java/edu/example/bitter/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package edu.example.bitter.repos;
+
+import edu.example.bitter.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
