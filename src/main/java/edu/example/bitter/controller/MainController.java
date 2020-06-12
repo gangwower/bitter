a527dcd57184b76b84a2b27f4e162678fa127e94
gangwower <gangwower@gmail.com>
2020-06-12 14:34:42 +0700

    Added author for all messages

--- src/main/java/edu/example/bitter/controller/MainController.java
+++ src/main/java/edu/example/bitter/controller/MainController.java
@@ -1,8 +1,10 @@
 package edu.example.bitter.controller;
 
 import edu.example.bitter.domain.Message;
+import edu.example.bitter.domain.User;
 import edu.example.bitter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,9 +29,14 @@ public class MainController {
         model.put("messages", messages);
         return "main";
     }
+
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model
+    ){
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
