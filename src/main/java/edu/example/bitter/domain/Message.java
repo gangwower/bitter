7e2e41361c5607bcc40e4341993a562e78c84eac
gangwower <gangwower@gmail.com>
2020-08-17 16:15:25 +0700

    Added bean validation, password encryption Added reCaptcha, rest client, remember me, Spring Session

--- src/main/java/edu/example/bitter/domain/Message.java
+++ src/main/java/edu/example/bitter/domain/Message.java
@@ -1,13 +1,20 @@
 package edu.example.bitter.domain;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Message {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Please fill the message")
+    @Length(max = 2048, message = "Message too long(>2048)")
     private String text;
+    @Length(max = 255, message = "Tag too long(>255)")
     private String tag;
 
     @ManyToOne(fetch = FetchType.EAGER)
--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
 insert into usr (id, username, password, active)
-    values (1, 'admin', '123', true);
+    values (1, 'admin', '$2y$08$gKzE0rNWJGEB9QoN78jxt.uqd5GAGbz59bWd/8.W.ph4vCukFhquW', true);
 
 insert into user_role (user_id, roles)
     values (1, 'USER'), (1, 'ADMIN');
